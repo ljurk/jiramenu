5ee6f17f49328e16812aa79618cd81dc2d07d805
ljurk <ljurk@pm.me>
2020-03-21 01:13:51 +0100

    add package stuff

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='jiramenu',
+    version='0.0.1',
+    packages=['jiramenu'],
+    author="Lukas Jurk",
+    author_email="ljurk@pm.me",
+    description="dmenu/rofi frontend for jira",
+    long_description=open('readme.md').read(),
+    long_description_content_type="text/markdown",
+    license="GPLv3",
+    keywords="jira dmenu rofi",
+    url="https://github.com/ljurk/jiramenu",
+    entry_points={
+        'console_scripts': ['jiramenu=jiramenu.jiramenu:cli']
+    },
+    install_requires=[
+        "jira",
+        "python-rofi",
+        "requests",
+        "Click"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ]
+)

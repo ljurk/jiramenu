a2878d73bb5bf5232d9782829b2b6d0856050df0
ljurk <ljurk@pm.me>
2020-03-23 07:36:59 +0100

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jiramenu',
-    version='1.0.4',
+    version='1.0.5',
     packages=['jiramenu'],
     author="Lukas Jurk",
     author_email="ljurk@pm.me",
